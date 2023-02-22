e0bdb69dbb991dd4a8da7a38ede768b9bb02fdbc
mehrani <mehraniali8002@gmail.com>
2023-02-23 01:45:59 +0330

    removed unnecessary codes

--- Code/main.py
+++ Code/main.py
@@ -45,41 +45,31 @@ print('Reading CSV file into dataframe and showing contents...')
 trainDataFrame = pd.read_csv(r'../train.csv')
 print(trainDataFrame)
 
-print('\n---------------------\n')
-
-print('Showing train csv file info using dataframe method info()')
+print('\n\nShowing train csv file info using dataframe method info()')
 trainDataFrame.info()
 
-print('\n---------------------\n')
-
-print('Showing first 5 rows of train dataframe using head() method.')
+print('\n\nShowing first 5 rows of train dataframe using head() method.')
 print(trainDataFrame.head())
 
-print('\n---------------------\n')
-
-print('Showing last 5 rows of train dataframe using head() method.')
+print('\n\nShowing last 5 rows of train dataframe using head() method.')
 print(trainDataFrame.tail())
 
-print('\n---------------------\n')
-print('Showing a general description of the train dataframe using describe() method...')
+print('\n\nShowing a general description of the train dataframe using describe() method...')
 print(trainDataFrame.describe())
 
 # PART 2
-print('\n---------------------\n')
 
 trainDFNumericalColNames = trainDataFrame.select_dtypes(include=numpy.number).columns.to_list()
-print('Columns that have numerical data :')
+print('\n\nColumns that have numerical data :')
 print(trainDFNumericalColNames)
 
 trainDFCategorialColNames = trainDataFrame.select_dtypes(exclude=["number","bool_"]).columns.to_list()
 print('Columns that have categorial data :')
 print(trainDFCategorialColNames)
 
-print('\n Changing male/female in train sataframe to 0/1 : \nnew train dataframe : \n')
+print('\n Changing male/female in train dataframe to 0/1 : \nnew train dataframe : \n')
 trainDataFrame = trainDataFrame.replace(['Male', 'Female'] , [0, 1])
 print(trainDataFrame)
-
-print('\n---------------------\n')
 
 #PART 3
 
@@ -90,8 +80,6 @@ print('\n\nReplacing NaN values with avg of corresponding column :')
 trainDataFrame['workclass'].fillna(value=trainDataFrame['workclass'].mode()[0], inplace=True)
 trainDataFrame['occupation'].fillna(value=trainDataFrame['occupation'].mode()[0], inplace=True)
 trainDataFrame['native-country'].fillna(value=trainDataFrame['native-country'].mode()[0], inplace=True)
-# print('Number of NaN for each column :\n')
-# print(trainDataFrame.isnull().sum(axis = 0))
 
 #PART 4 
 
